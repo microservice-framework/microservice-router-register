8a3c90b3fd087110317980c67f66b02180b29934
Gor Martsen <Gor.martsen@gmail.com>
2017-04-19 21:06:21 -0400

    Clean up and event driven now.

--- index.js
+++ index.js
@@ -4,11 +4,12 @@
  */
 'use strict';
 
+const EventEmitter = require('events').EventEmitter;
+const util = require('util');
 const MicroserviceClient = require('@microservice-framework/microservice-client');
-const pidusage = require( 'pidusage' );
-const os = require( 'os' );
-
-const bind = function(fn, me) { return function() { return fn.apply(me, arguments); }; };
+const pidusage = require('pidusage');
+const os = require('os');
+const debug = require('debug');
 
 /**
  * Constructor of ZenciMicroservice object.
@@ -18,97 +19,106 @@ const bind = function(fn, me) { return function() { return fn.apply(me, argument
  *   settings.server.period = Send request each milisec
  *   settings.route.url = Self URL to register http(s)://IP:PORT;
  *   settings.route.path = URL base path to register with
- *   settings.type = Is it master or child.
+ *   settings.cluster = cluster info.
  */
 function MicroserviceRouterRegister(settings) {
+  EventEmitter.call(this);
 
   // Use a closure to preserve `this`
   var self = this;
-
   self.settings = settings;
+  self.cluster = settings.cluster;
+  self.server = settings.server;
+  self.route = settings.route;
+  self.authData = false;
 
-  self.register = bind(self.register, self);
-  self.monitor = bind(self.monitor, self);
-  self.register(settings);
-
-}
-
-/**
- * Ping server by timer.
- */
-MicroserviceRouterRegister.prototype.monitor = function(cluster, client, RecordID, RecordToken) {
-
-  var totalWorkers = 0 ;
-  for (var id in cluster.workers) {
-    totalWorkers++;
-  }
-
-  var receivedStats = [];
-
-  for (var id in cluster.workers) {
-    pidusage.stat( cluster.workers[id].process.pid, function( error, stat) {
-      stat.memory = stat.memory / 1024 / 1024;
-      stat.loadavg = os.loadavg();
-      receivedStats.push(stat);
-
-      if(receivedStats.length == totalWorkers ) {
-        client.put(RecordID, RecordToken, { metrics: receivedStats}, function(err, handlerResponse) {
-          if (err) {
-            console.log('Router server report error %s.', err.message);
-          }
-        });
-      }
-    });
-  }
-}
-
-/**
- * Register route.
- */
-MicroserviceRouterRegister.prototype.register = function(settings) {
-  var self = this;
-
-  var client = new MicroserviceClient({
-    URL: settings.server.url,
-    secureKey: settings.server.secureKey
+  self.client = new MicroserviceClient({
+    URL: self.server.url,
+    secureKey: self.server.secureKey
   });
 
-  var totalWorkers = 0 ;
-  for (var id in self.settings.cluster.workers) {
-    totalWorkers++;
-  }
+  self.on('timer', function() {
+    self.collectStats();
+  });
+
+  self.on('report', function(stats) {
+    self.reportStats(stats);
+  });
+
+  setInterval(function() {
+    self.emit('timer');
+    self.debug.debug('timer triggered');
+  }, self.server.period);
+
+  self.collectStats();
+}
+
+util.inherits(MicroserviceRouterRegister, EventEmitter);
+
+MicroserviceRouterRegister.prototype.debug = {
+  log: debug('microservice-router-register:log'),
+  debug: debug('microservice-router-register:debug'),
+};
+
+
+/**
+ * Collect Stats.
+ */
+MicroserviceRouterRegister.prototype.collectStats = function() {
+  var self = this;
+
+  self.debug.debug('collect stats');
   var receivedStats = [];
 
-  for (var id in self.settings.cluster.workers) {
-    pidusage.stat( self.settings.cluster.workers[id].process.pid, function( error, stat) {
+  var workersCount = 0;
+  for (var i in self.cluster.workers) {
+    workersCount = workersCount + 1;
+  }
+
+  for (var id in self.cluster.workers) {
+    pidusage.stat(self.cluster.workers[id].process.pid, function(error, stat) {
       stat.memory = stat.memory / 1024 / 1024;
       stat.loadavg = os.loadavg();
       receivedStats.push(stat);
-
-      if(receivedStats.length == totalWorkers ) {
-        client.post({
-            url: settings.route.url,
-            path: settings.route.path,
-            metrics: receivedStats,
-            secureKey: settings.route.secureKey
-          },
-          function(err, handlerResponse) {
-            if (!err) {
-              setInterval(self.monitor,
-                settings.server.period,
-                self.settings.cluster,
-                client,
-                handlerResponse.id,
-                handlerResponse.token
-              );
-            } else {
-              console.log('Router server is not available.')
-            }
-          }
-        );
+      if (receivedStats.length == workersCount) {
+        self.emit('report', receivedStats);
       }
     });
   }
-};
+}
+
+/**
+ * Report Stats.
+ */
+MicroserviceRouterRegister.prototype.reportStats = function(stats) {
+  var self = this;
+
+  self.debug.debug('report stats');
+  if (!self.authData) {
+    self.debug.debug('register on router');
+    self.client.post({
+      url: self.route.url,
+      path: self.route.path,
+      metrics: stats,
+      secureKey: self.route.secureKey
+    },
+    function(err, handlerResponse) {
+      if (!err) {
+        self.authData = handlerResponse;
+      } else {
+        self.debug.log('Router server is not available.')
+      }
+    });
+    return;
+  }
+  self.debug.debug('Update stats on router');
+  self.client.put(self.authData.id, self.authData.token,
+    { metrics: stats}, function(err, handlerResponse) {
+    if (err) {
+      self.authData = false;
+      return self.reportStats(stats);
+    }
+  });
+}
 
 module.exports = MicroserviceRouterRegister;
